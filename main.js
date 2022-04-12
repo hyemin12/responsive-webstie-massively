5b84dccd8aa98b9db57b98f07860c3eef3620484
hyemin12 <khnm31@naver.com>
2022-04-12 18:56:08 +0900

    '모바일nav클릭수정'

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const toggle = document.querySelector(".toggle");
 const btnClose = document.querySelector(".btn-close");
+const toggleNav = document.querySelector(".toggle-nav");
 
 function handleToggle() {
-  const toggleNav = document.querySelector(".toggle-nav");
   if (toggleNav.classList.value.includes("view")) {
     toggleNav.classList.remove("view");
   } else {
@@ -25,9 +25,11 @@ function handleLogo() {
   if (window.scrollY >= 400) {
     intro.classList.add("hidden");
     logo.classList.add("view");
+    toggle.classList.add("scroll");
   } else if (window.scrollY < 400) {
     intro.classList.remove("hidden");
     logo.classList.remove("view");
+    toggle.classList.remove("scroll");
   }
   logo.addEventListener("click", function () {
     window.scrollTo({ top: 0, behavior: "smooth" });
@@ -36,16 +38,21 @@ function handleLogo() {
 window.addEventListener("scroll", handleLogo);
 
 const tabs = document.querySelectorAll(".tab-nav li");
+const mTabs = document.querySelectorAll(".mobile-nav li");
 function handleTab(e) {
   const tabPosts = document.querySelectorAll(".main > div");
   for (let i = 0; i < 3; i++) {
     tabPosts[i].classList.add("hidden");
     tabs[i].classList.remove("active");
     e.target.classList.add("active");
+    toggleNav.classList.remove("view");
   }
   const index = e.target.classList[0];
   document.getElementById(`${index}`).classList.remove("hidden");
 }
 tabs.forEach((tab) => {
+  tab.addEventListener("click", handleTab);
+});
+mTabs.forEach((tab) => {
   tab.addEventListener("click", handleTab);
 });
